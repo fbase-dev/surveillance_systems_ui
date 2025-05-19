c2663dfc100e7a26dd076328ce7b5fc22fb36338
CotterellAnna <cotterellanna@gmail.com>
2025-05-19 15:06:01 +0100

    radio

---
+++ src/components/Dashboard/FrequencyCard.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@mantine/core"
+import FrequencyControl from "../FrequencyControl"
+
+export default function FrequencyCard(){
+    return(
+        <Card mih={"10vh"}>
+            <FrequencyControl />
+        </Card>
+    )
+}
